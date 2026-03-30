3d13504bc8237573892466c31a0b090cdec80a8b
roddyndego1 <roddy.ndego@acity.edu.gh>
2026-03-30 12:57:54 +0000

    Update myApp.js
    mounted helmet.frameguard and set action to 'DENY'

--- myApp.js
+++ myApp.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-app.use(helmet.hidePoweredBy());
-
+app.use(helmet.frameguard({action: 'DENY'}));
 
 
 
